d235ca4c5b993d3577512582f8e3fc3f01b85664
Filipe de Castro <filipeyoga@gmail.com>
2020-11-04 15:06:23 -0300

    chore: remove unnecessary variable

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 
 global.SALT_KEY = "f5b99242-6504-4ca3-90f2-05e78e5761ef";
 global.EMAIL_TMPL = "Hello <strong>{0}</strong>, welcome to The Wall!";
@@ -6,5 +6,4 @@ global.EMAIL_TMPL = "Hello <strong>{0}</strong>, welcome to The Wall!";
 module.exports = {
   connectionString: process.env.CONNECTION_STRING,
   sendgridKey: process.env.SENDGRID_KEY,
-  containerConnectionString: process.env.CONTAINER_KEY,
 };
