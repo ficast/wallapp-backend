8e4e514a4cc86abea106abf264c768cdcd0c86bc
Filipe de Castro <filipeyoga@gmail.com>
2021-01-22 23:21:19 -0300

    Refactor: change md5 for AES

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const config = require("./config");
-const cors = require('cors')
-
+const cors = require("cors");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 const router = express.Router();
 
 // Database
 try {
-  mongoose.connect(config.connectionString,{
+  mongoose.connect(
+    config.connectionString,
+    {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
@@ -27,20 +28,24 @@ try {
 
 // Models
 require("./models/user");
-require('./models/post');
+require("./models/post");
 
 // Routes
 const indexRoute = require("./routes/index-route");
 const userRoute = require("./routes/user-route");
 const postRoute = require("./routes/post-route");
 
-app.use(bodyParser.json({
-  limit: '5mb'
-}));
+app.use(
+  bodyParser.json({
+    limit: "5mb",
+  })
+);
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 app.use("/", indexRoute);
 app.use("/user", userRoute);
--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -2,9 +2,8 @@
 
 const ValidationContract = require("../validators/fluent-validator");
 const repository = require("../repositories/user-repository");
-const md5 = require("md5");
+const CryptoJS = require("crypto-js");
 const authService = require("../services/auth-service");
-
 const emailService = require("../services/email-service");
 
 exports.get = async (req, res, next) => {
@@ -38,11 +37,15 @@ exports.post = async (req, res, next) => {
     return;
   }
 
+  const password = CryptoJS.AES.encrypt(
+    req.body.password + global.SALT_KEY
+  ).toString();
+
   try {
     await repository.create({
       name: req.body.name,
       email: req.body.email,
-      password: md5(req.body.password + global.SALT_KEY),
+      password,
       roles: ["user"],
     });
 
@@ -63,10 +66,14 @@ exports.post = async (req, res, next) => {
 };
 
 exports.authenticate = async (req, res, next) => {
+  const password = CryptoJS.AES.encrypt(
+    req.body.password + global.SALT_KEY
+  ).toString();
+
   try {
     const user = await repository.authenticate({
       email: req.body.email,
-      password: md5(req.body.password + global.SALT_KEY),
+      password,
     });
 
     if (!user) {
